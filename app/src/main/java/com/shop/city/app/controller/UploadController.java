6ef025472a375fb6a842a0f83c1269beb659ae02
guoqianhua <guoqianhua@yegoo.com>
2020-06-29 17:31:06 +0800

    [Type]: Improvement
    [JIRA-ID]:
    
    [Description]: 优化

--- app/src/main/java/com/shop/city/app/controller/UploadController.java
+++ app/src/main/java/com/shop/city/app/controller/UploadController.java
@@ -6,12 +6,17 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import com.shop.city.common.service.UploadService;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
+@RequestMapping("/common/v")
 public class UploadController {
 
     @Autowired
