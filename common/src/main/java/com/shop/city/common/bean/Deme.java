b0a7f1ee88a8c438bcd9e598a996981cc88f02cb
guoqianhua <guoqianhua@yegoo.com>
2020-07-29 10:11:28 +0800

    [Type]: Other
    [JIRA-ID]:
    
    [Description]: maven配置

---
+++ common/src/main/java/com/shop/city/common/bean/Deme.java
@@ -0,0 +1,13 @@
+package com.shop.city.common.bean;
+
+import lombok.Data;
+
+@Data
+public class Deme {
+
+    private String ss;
+
+    public Deme(String ss){
+        ss = this.ss;
+    }
+}
---
+++ common/src/main/java/com/shop/city/common/bean/Testddd.java
@@ -0,0 +1,37 @@
+//package com.shop.city.common.bean;
+//
+//import java.math.BigDecimal;
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Set;
+//import java.util.stream.Collectors;
+//
+//public class Testddd {
+//
+//    public static void main(String[] args) {
+//        double i = 100;
+//        double n = 3;
+//
+//        BigDecimal a = new BigDecimal(100);
+//        BigDecimal b = new BigDecimal(3);
+//
+////        System.out.println(i/n);
+//        /**
+//         * 保留7位小数，四舍五入
+//         */
+////        System.out.println(a.divide(b,7,BigDecimal.ROUND_HALF_UP));
+//
+//        List<String> ss = new ArrayList<>();
+//        ss.add("ss");
+//        ss.add("ss");
+//        ss.add("ss");
+//        ss.add("aa");
+//        long count = ss.stream().distinct().count();
+//
+//        List<String> distinctElements = ss.stream().distinct().collect(Collectors.toList());
+//
+//        System.out.println("count:"+count);
+//        System.out.println("size:"+distinctElements.size());
+//    }
+//}
